acd574a2a902ac54be1889b2a1d5805c4d8243f1
Ivan Rodrigues <ivanknow@gmail.com>
2015-03-09 17:40:59 -0300

    teste ajax

--- provamovelserverplay/app/controllers/Application.java
+++ provamovelserverplay/app/controllers/Application.java
@@ -1,18 +1,51 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.HashMap;
 
-import views.html.*;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.ajax;
+import views.html.index;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
-
+	static Form form = new Form(Object.class);
     public static Result index() {
         return ok(index.render("Home","Seja bem vindo ao sistema.","Para cadastar suas provas se cadastre no menu superior"));
     }
     
     public static Result debug(String s) {
         return ok(index.render("Home","Seja bem vindo ao sistema.",s));
+    }
+    
+    public static Result ajax() {
+    	
+    	if (!form.hasErrors()) {
+    	
+    		DynamicForm requestData = form.form().bindFromRequest();
+			HashMap values = (HashMap) requestData.get().getData();
+			
+			ObjectNode result = Json.newObject();
+			
+			result.put("campos",(String)values.get("test"));
+			
+			/*result.put("exampleField2", "Hello world!");*/
+			
+    		return ok(result);
+        	
+    	}
+    	return redirect(routes.Application.debug(form.errorsAsJson().asText()));
+    	
+    }
+    
+ public static Result test() {
+    	
+    	return ok(ajax.render());
+    	
     }
 
 }
